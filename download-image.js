d74cae075df8165812c3c8f9aea153d2250a0671
Brenton Holswich <brenton.holswich@gmail.com>
2017-03-14 00:15:30 +0000

    changed the methods to chain and added an on finish for when the write is complete

--- download-image.js
+++ download-image.js
@@ -1,20 +1,21 @@
-// Import the request object from our node_modules
+// Import the request object from our node_modules and fs from node
+let fs = require('fs');
 let request = require('request');
 
-request('https://sytantris.github.io/http-examples/future.jpg').pipe(fs.createWriteStream('./future.jpg'));
+request('https://sytantris.github.io/http-examples/future.jpg')
+        .pipe(fs.createWriteStream('./future.jpg'))
+        .on('error', function(err) {
+          console.log('Error:', err);
+          return false;
+        })
+        .on('response', function(res) {
+          console.log('Response Code:', res.statusCode);
+          console.log('Response Message:', res.statusMessage);
+          console.log('Content Type:', res.headers['content-type']);
+        })
+        .on('finish', function() {
+          console.log('Download Complete');
+        });
+
 console.log('Downloading Image...');
 
-request.on('error', function(err) {
-  console.log('Error:', err);
-  return false;
-});
-
-request.on('response', function(res) {
-  console.log('Response Code:', res.statusCode);
-  console.log('Response Message:', res.statusMessage);
-  console.log('Content Type:', res.headers['content-type']);
-});
-
-request.on('finish', function() {
-  console.log('Download Complete');
-});
